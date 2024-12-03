1125033d467360ece53d0864a8595b9d22ca97bb
StepLeo <stepleo66@gmail.com>
2024-12-03 19:48:48 +0100

    Encoders for VGG, ResNet and UNet Classification heads missing

--- models/unet.py
+++ models/unet.py
@@ -1,10 +1,26 @@
+import torch.nn as nn
+from layers import conv_block, decoder_block
 from resnet import ResNet
 
 class UNet(ResNet):
+    """
+    UNet inheriting from a ResNet encoder.
+    """
     def __init__(self):
         super(UNet, self).__init__()
-        # Define UNet architecture here
+        self.b = conv_block(512, 1024, 1)
+        self.d1 = decoder_block(1024, 512)
+        self.d2 = decoder_block(512, 256)
+        self.d3 = decoder_block(256, 128)
+        self.d4 = decoder_block(128, 64)
+        self.outputs = nn.Conv2d(64, 1, kernel_size=1, padding=0)
 
-    def forward(self, x):
-        # Define forward pass
-        return x
+    def forward(self, inputs):
+        p, skip_list = self.encoder(inputs)
+        b = self.b(p)
+        d1 = self.d1(b, skip_list[3])
+        d2 = self.d2(d1, skip_list[2])
+        d3 = self.d3(d2, skip_list[1])
+        d4 = self.d4(d3, skip_list[0])
+        outputs = self.outputs(d4)
+        return outputs
--- models/vgg.py
+++ models/vgg.py
@@ -1,10 +1,34 @@
 import torch.nn as nn
+from layers import conv_block
 
 class VGG(nn.Module):
-    def __init__(self):
+    """
+    Follows the VGG_19 architecture described in the following list:
+    [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 256, 'M', 512, 512, 512, 512, 'M', 512, 512, 512, 512, 'M']
+    """
+    def __init__(self, input_img_c):
         super(VGG, self).__init__()
-        # Define VGG architecture here
+        self.conv_block_1 = conv_block(in_c=input_img_c, out_c=64, n_conv=2)
+        self.conv_block_2 = conv_block(in_c=64, out_c=128, n_conv=2)
+        self.conv_block_3 = conv_block(in_c=128, out_c=256, n_conv=4)
+        self.conv_block_4 = conv_block(in_c=256, out_c=512, n_conv=4)
+        self.conv_block_5 = conv_block(in_c=512, out_c=512, n_conv=4)
+        # Max pooling for encoding
+        self.pool = nn.MaxPool2d((2, 2))
+        
 
-    def forward(self, x):
-        # Define forward pass
-        return x
+    def forward(self, inputs):
+        x1 = self.conv_block_1(inputs)
+        p1 = self.pool(x1)
+        x2 = self.conv_block_2(p1)
+        p2 = self.pool(x2)
+        x3 = self.conv_block_3(p2)
+        p3 = self.pool(x3)
+        x4 = self.conv_block_4(p3)
+        p4 = self.pool(x4)
+        x5 = self.conv_block_5(p4)
+        p5 = self.pool(x5)
+
+        #TODO: Add classifcation layer
+        
+        return 
